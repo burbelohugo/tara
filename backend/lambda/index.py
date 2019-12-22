de55c4800643a95d222b33906fb9e20886ff27a8
Hugo Burbelo <hburbelo@phone2action.com>
2019-12-22 13:58:53 -0500

    Get login automation working

--- backend/lambda/index.py
+++ backend/lambda/index.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.common.keys import Keys
+from env import USER_EMAIL, USER_PASSWORD
 PAGE_BASE_URL = "https://esjbooked.umd.edu/Web/index.php?redirect="
+
 
 def handler(event, context):
     options = Options()
@@ -16,8 +18,20 @@ def handler(event, context):
     driver.get(PAGE_BASE_URL)
     body = f"Headless Chrome Initialized, Page title: {driver.title}"
 
-    driver.close();
-    driver.quit();
+    inputElement = driver.find_element_by_id("email")
+    inputElement.send_keys(USER_EMAIL)
+
+    inputElement = driver.find_element_by_id("password")
+    inputElement.send_keys(USER_PASSWORD)
+
+    driver.find_element_by_css_selector('#login-box > div:nth-child(5) > button').click()
+
+    body = {
+        "result": driver.find_element_by_id("schedule-title").get_attribute('innerHTML')
+    }
+
+    driver.close()
+    driver.quit()
 
     response = {
         "statusCode": 200,
